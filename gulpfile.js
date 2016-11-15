d7ac00472d11521879dac775acfd586460712f0b
sakoh <sakohartounian@yahoo.com>
2016-11-14 17:02:33 -0800

    fonts now work for good

--- gulpfile.js
+++ gulpfile.js
@@ -45,21 +45,11 @@ gulp.task('copy', function() {
     gulp.src(['node_modules/jquery/dist/jquery.js', 'node_modules/jquery/dist/jquery.min.js'])
         .pipe(gulp.dest('dist/vendor/jquery'))
 
-    gulp.src([
-            'node_modules/font-awesome/**',
-            '!node_modules/font-awesome/**/*.map',
-            '!node_modules/font-awesome/.npmignore',
-            '!node_modules/font-awesome/*.txt',
-            '!node_modules/font-awesome/*.md',
-            '!node_modules/font-awesome/*.json'
-        ])
-        .pipe(gulp.dest('dist/vendor/font-awesome'))
-
+    gulp.src(['node_modules/font-awesome/fonts/**/*'])
+        .pipe(gulp.dest('dist/fonts'))
+        
     gulp.src('img/**/*')
-        .pipe(gulp.dest('dist/img'));
-
-    gulp.src('fonts/**/*')
-        .pipe(gulp.dest('dest/fonts'));
+        .pipe(gulp.dest('dist/img'))
 
 })
 
