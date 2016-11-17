7def6518953ebbb5c74597f13f4f5417f44c8395
sakoh <sakohartounian@yahoo.com>
2016-11-16 17:24:55 -0800

    data simplified

--- data/index.js
+++ data/index.js
@@ -1,17 +1,3 @@
-var fs = require('fs'),
-    gm = require('gray-matter'),
-    path = require('path'),
-    marked = require('marked');
-
-function byID(a,b) {
-  if (a.id < b.id)
-    return -1;
-  if (a.id > b.id)
-    return 1;
-  return 0;
-}
-
-
 module.exports = {
   site: {
     name: "Rent A Tool",
