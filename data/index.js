9da4cc83271da3e38228ef99b12a7697b9eb78a3
sakoh <sakohartounian@yahoo.com>
2016-11-18 09:12:14 -0800

    portfolio made into a loop

--- data/index.js
+++ data/index.js
@@ -23,5 +23,67 @@ module.exports = {
         url: "#map"
       }
     ]
-  }
+  },
+  gallery: [
+    {
+      img_name: "01",
+      name: "Project Title",
+      category: "residential"
+    },
+    {
+      img_name: "02",
+      name: "Project Title",
+      category: "commericial"
+    },
+    {
+      img_name: "03",
+      name: "Project Title",
+      category: "residential"
+    },
+    {
+      img_name: "04",
+      name: "Project Title",
+      category: "residential"
+    },
+    {
+      img_name: "05",
+      name: "Project Title",
+      category: "commercial"
+    },
+    {
+      img_name: "06",
+      name: "Project Title",
+      category: "office"
+    },
+    {
+      img_name: "07",
+      name: "Project Title",
+      category: "office"
+    },
+    {
+      img_name: "08",
+      name: "Project Title",
+      category: "commercial"
+    },
+    {
+      img_name: "09",
+      name: "Project Title",
+      category: "commercial"
+    },
+    {
+      img_name: "10",
+      name: "Project Title",
+      category: "office"
+    },
+    {
+      img_name: "11",
+      name: "Project Title",
+      category: "office"
+    },
+    {
+      img_name: "12",
+      name: "Project Title",
+      category: "residential"
+    }
+  ]
 };
